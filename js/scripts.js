e59c731c802e719bac0493c0880ba1601dc2f5b2
Bukisani <103451235+Bukisani@users.noreply.github.com>
2022-04-15 10:37:03 -0500

    Update scripts.js
    Created a new variable called pokemonList and added objects in it.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('hello world!');
-let favoriteFood = 'pizza';
-document.write('My favorite food is' + ' ' + favoriteFood);
+let pokemonList = [
+  { name: 'charmander', height: 0.6, types: ['monster', 'dragon'] },
+  { name: 'Squirtle', height: 0.5, types: ['monster', 'water'] },
+  { name: 'beedrill', height: 1, types: 'bug' },
+  { name: 'raticate', height: 0.7, types: 'field' },
+  { name: 'nidoking', height: 1.4, types: ['monster', 'field'] },
+];
