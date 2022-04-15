dc1ae7c5cabe6c6b9431fc47298b5e1e7e474da5
Bukisani <103451235+Bukisani@users.noreply.github.com>
2022-04-15 16:06:28 -0500

    Update scripts.js
    added the for loop that iterates over each item in the pokemonList and an If condition that checks if the height is greater than 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
+// creates a new variable pokemonList with objects in the array
 let pokemonList = [
-  { name: 'charmander', height: 0.6, types: ['monster', 'dragon'] },
+  { name: 'Harmander', height: 0.6, types: ['monster', 'dragon'] },
   { name: 'Squirtle', height: 0.5, types: ['monster', 'water'] },
-  { name: 'beedrill', height: 1, types: 'bug' },
-  { name: 'raticate', height: 0.7, types: 'field' },
-  { name: 'nidoking', height: 1.4, types: ['monster', 'field'] },
+  { name: 'Beedrill', height: 1, types: ['bug'] },
+  { name: 'Raticate', height: 0.7, types: ['field'] },
+  { name: 'Nidoking', height: 1.4, types: ['monster', 'field'] },
 ];
+// the for loop iterates over each item in the pokemonList
+for (let i = 0; i < pokemonList.length; i++) {
+  //Condition that checks if the height is greater than 1
+  if (pokemonList[i].height > 1) {
+    document.write(
+      `${pokemonList[i].name} (height:${pokemonList[i].height}) Wow, that\'s big!<br> `
+    );
+  } else {
+    document.write(
+      `${pokemonList[i].name} (height:${pokemonList[i].height})<br> `
+    );
+  }
+}
